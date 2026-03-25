013a667928303e79b6fa21c8087512eae2ce10cd
Mocchi <mikolajgolonka43@gmail.com>
2026-03-25 13:11:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 
 const oblicz = document.querySelector('button')
+const kursy = document.querySelector("input[type='checkbox']")
 
 function funkcja_obliczeniowa(event){
+
+    
+
     event.preventDefault()
 
     const checkboxy = document.querySelectorAll("input[type='checkbox']:checked")
@@ -12,17 +16,34 @@ function funkcja_obliczeniowa(event){
 
     const tabela = document.querySelector('table')    // tabela którą zrobię
 
+    let suma = 0
+
     tabela.innerHTML = `<tr>
-                    <th>kurs</th>
-                    <th>kwota</th>
-                </tr>`
+        <th>kurs</th>
+        <th>kwota</th>
+    </tr>`
 
     for(let kurs of checkboxy){
-        tabela.innerHTML += `<tr>
-            <td>${kurs.name}</td>
-            <td>${kurs.value}</td>
-        </tr>`    
+        if(kurs.checked){
+            tabela.innerHTML += `<tr>
+                <td>${kurs.name}</td>
+                <td>${kurs.value}</td>
+            </tr>`
+            
+            suma += Number(kurs.value)
+        }
     }
+
+
+    const raty_span = document.querySelector('')
+    const suma_span = document.querySelector('')
+
+    raty_span.textContent = ilosc_rat
+    suma_span.textContent = Number(suma / ilosc_rat).toFixed(2)
+
+
 }
+
+
 
 oblicz.addEventListener('click', funkcja_obliczeniowa)
